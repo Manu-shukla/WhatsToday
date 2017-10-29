9046b65852ea74aa52dbd7a90f62f6c89414e809
srishti109 <32215683+srishti109@users.noreply.github.com>
2017-10-29 19:39:51 +0530

    Update todos.js
    added new comment

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,14 +3,14 @@ $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-//Click on X to delete Todo
+//click on X to delete todo
 $("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
-
+//checks if key pressed is 13 or not
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		//grabbing new todo text from input
